9d4e6f80e9d828f1e708c91d806bba097e80adab
Odilomar Rebelo Rocha Junior <odilomar.junior@gmail.com>
2020-07-25 12:10:54 -0400

    :lipstick: Change background and layout from Login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,108 +1,112 @@
 import React, { useState, ChangeEvent } from "react";
 import { useHistory } from "react-router-dom";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import api from "../../service/api";
 
+import {
+  Title,
+  Form,
+  Div,
+  Subtitle,
+  Input,
+  Select,
+  Options,
+  Button,
+} from "./styles";
+
 interface User {
-    name: string;
-    role: string;
+  name: string;
+  role: string;
 }
 
 interface SelectOption {
-    value: string;
-    label: string;
+  value: string;
+  label: string;
 }
 
 const Login = () => {
+  const [user, setUser] = useState<User>({
+    name: "",
+    role: "admin",
+  });
 
-    const [user, setUser] = useState<User>({
-        name: '',
-        role: '',
-    });
+  const options: SelectOption[] = [
+    { value: "admin", label: "Administrador" },
+    { value: "tecnico", label: "Técnico" },
+    { value: "super", label: "Super Usuário" },
+  ];
 
-    const options: SelectOption[] = [
-        { value: "tecnico", label: "Técnico" },
-        { value: "super", label: "Super Usuário" },
-        { value: "admin", label: "Administrador do Sistema" },
-    ];
+  const history = useHistory();
 
-    const history = useHistory();
+  const handleInputChange = (inputName: ChangeEvent<HTMLInputElement>) => {
+    const { value } = inputName.target;
 
-    const handleInputChange = (inputName: ChangeEvent<HTMLInputElement>) => {
-        const { value } = inputName.target;
+    updateUser(value, user.role);
+  };
 
-        updateUser(value, user.role);
+  const handleSelectChange = (inputSelect: ChangeEvent<HTMLSelectElement>) => {
+    const { value } = inputSelect.target;
+
+    updateUser(user.name, value);
+  };
+
+  const updateUser = (name: string, role: string) => {
+    setUser({ name, role });
+  };
+
+  const signIn = async () => {
+    if (user.name === "" || user.role === "") {
+      alert("Dados inválidos. Corrija os dados e Tente novamente!");
+      return;
     }
 
-    const handleSelectChange = (inputSelect: ChangeEvent<HTMLSelectElement>) => {
-        const { value } = inputSelect.target;
+    const data = { Nome: user.name, Role: user.role };
 
-        updateUser(user.name, value);
+    const loginResponse = await api.post("/v1/account/login", data);
+
+    if (loginResponse.status === 404) {
+      alert("Usuário não encontrado. Verifique seus dados e tente novamente!");
+      return;
     }
 
-    const updateUser = (name: string, role: string) => {
-        setUser({ name, role });
+    if (loginResponse.status === 500) {
+      alert("Problemas com o servidor. Tente novamente mais tarde!");
+      return;
     }
 
-    const signIn = async () => {
+    const { token } = loginResponse.data;
 
-        if (user.name === '' || user.role === '') {
-            alert("Dados inválidos. Corrija os dados e Tente novamente!");
-            return;
-        }
+    window.localStorage.setItem("token", token);
 
-        const data = { "Nome": user.name, "Role": user.role };
+    history.push("/dashboard");
+  };
 
-        const loginResponse = await api.post('/v1/account/login', data);
+  return (
+    <>
+      <Title>Sistema de Segurança Digital</Title>
 
-        if (loginResponse.status === 404) {
-            alert("Usuário não encontrado. Verifique seus dados e tente novamente!");
-            return;
-        }
-
-        if (loginResponse.status === 500) {
-            alert("Problemas com o servidor. Tente novamente mais tarde!");
-            return;
-        }
-
-        const { token } = loginResponse.data;
-
-        window.localStorage.setItem("token", token);
-
-        history.push('/dashboard');
-    }
-
-    return (
-        <div className="container h-100">
-            <div className="row h-100 justify-content-center align-items-center">
-                <div className="col-md-7">
-                    <h1 className="text-center">Sistema de Segurança Digital</h1>
-                    <form className="mt-4">
-                        <div className='form-group'>
-                            <label htmlFor="nome">Nome</label>
-                            <input
-                                type="text"
-                                id="nome"
-                                className="form-control"
-                                onChange={handleInputChange}
-                                placeholder="Informe seu nome"
-                            />
-                        </div>
-                        <div className='form-group'>
-                            <label htmlFor="role">Setor</label>
-                            <select name="" id="role" className="form-control" onChange={handleSelectChange}>
-                                {options.map(option => (
-                                    <option key={option.value} value={option.value}>{option.label}</option>
-                                ))}
-                            </select>
-                        </div>
-                        <button type="button" className="btn btn-primary btn-block" onClick={signIn}>Entrar</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    );
+      <Div>
+        <Form
+          onSubmit={(event) => {
+            event.preventDefault();
+            signIn();
+          }}
+        >
+          <Subtitle>Login</Subtitle>
+          <Input placeholder="Usuário" onChange={handleInputChange} />
+          <Select onChange={handleSelectChange}>
+            {options.map((option) => (
+              <Options key={option.value} value={option.value}>
+                {option.label}
+              </Options>
+            ))}
+          </Select>
+          <Button>ACESSAR</Button>
+        </Form>
+      </Div>
+    </>
+  );
 };
 
 export default Login;
---
+++ src/pages/Login/styles.ts
@@ -0,0 +1,95 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  font-size: 3rem;
+  font-weight: 300;
+  line-height: 3, 6875rem;
+  font-family: "Montserrat", sans-serif;
+
+  position: absolute;
+  top: 3rem;
+  right: 15rem;
+`;
+
+export const Div = styled.div`
+  position: absolute;
+  top: 35.79%;
+  right: 15rem;
+
+  max-width: 44rem;
+  width: 100%;
+`;
+
+export const Form = styled.form`
+  width: 50%;
+  margin-left: 25%;
+
+  text-align: center;
+`;
+
+export const Subtitle = styled.h2`
+  font-family: "Montserrat", sans-serif;
+  font-style: normal;
+  font-size: 1.75rem;
+  font-weight: normal;
+  line-height: 2rem;
+`;
+
+export const Input = styled.input.attrs((props) => ({
+  type: "text",
+}))`
+  background: #ffffff;
+  display: block;
+  padding: 0.5rem 0 0.5rem 1rem;
+  margin-top: 1rem;
+
+  border: 1px solid #828282;
+  box-sizing: border-box;
+  border-radius: 8px;
+
+  font-family: "Montserrat", sans-serif;
+  font-size: 1rem;
+  line-height: 1rem;
+
+  width: 100%;
+`;
+
+export const Select = styled.select`
+  background: #ffffff;
+  display: block;
+  padding: 0.5rem 0 0.5rem 1rem;
+  margin-top: 1rem;
+
+  border: 1px solid #828282;
+  box-sizing: border-box;
+  border-radius: 8px;
+
+  font-family: "Montserrat", sans-serif;
+  font-size: 1rem;
+  line-height: 1rem;
+
+  width: 100%;
+`;
+
+export const Options = styled.option``;
+
+export const Button = styled.button`
+  font-family: "Montserrat", sans-serif;
+  font-size: 1rem;
+  line-height: 1rem;
+
+  background: linear-gradient(
+    90deg,
+    #f2994a -2.86%,
+    #f54c8b 52.73%,
+    #9b51e0 108.33%
+  );
+
+  border-radius: 8px;
+  border-width: 0;
+  color: #ffffff;
+
+  width: 50%;
+  margin-top: 1rem;
+  padding: 0.75rem;
+`;
